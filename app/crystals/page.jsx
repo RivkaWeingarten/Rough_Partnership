fd1e7b3e1b45247d790135da011caf116b21cc5f
Rivka Weingarten <rw240brooklyn@gmail.com>
2024-07-10 23:07:13 -0400

    fetched resource now need options

--- app/crystals/page.jsx
+++ app/crystals/page.jsx
@@ -1,41 +1,22 @@
-import crystals from "@/crystals.json";
-// import CrystalTable from '@/components/CrystalTable';
+// import crystals from "@/crystals.json";
+import getResourceNumbers from "../actions/getResourceNumbers";
 import CrystalAccordion from "@/components/CrystalAccordion";
 
-// function CrystalsPage() {
-//   return (
-//     <div>
-   
-//       <section className="px-4 py-6">
-      
-//       <div className="container mx-auto px-4 py-6">
-//      {crystals.length===0 ? (<p>no diamonds in this lot</p>):(<div className="grid grid-cols-1 md:grid-cols-3 gap-6">
- 
-//  {crystals.map((crystal)=> (
-// <CrystalAccordion  key= {crystal._id} crystal={crystal} />
-// ))} 
-
-// </div>)}
-      
-//        </div>
-//        </section>
-//       </div>
-//   )
-// }
-
-// export default CrystalsPage;
-
-function CrystalsPage() {
+async function CrystalsPage() {
+  const { resourceNumbers, error } = await getResourceNumbers();
   return (
     <div>
       <section className="px-4 py-6">
         <div className="container mx-auto px-4 py-6">
-          {crystals.length === 0 ? (
+          {resourceNumbers.length === 0 ? (
             <p>No diamonds in this lot</p>
           ) : (
             <div className="grid grid-cols-1 gap-6">
-              {crystals.map((crystal) => (
-                <CrystalAccordion key={crystal._id} crystal={crystal} />
+              {resourceNumbers.map((resourceNumber) => (
+                <CrystalAccordion
+                  key={resourceNumber.id}
+                  resourceNumber={resourceNumber}
+                />
               ))}
             </div>
           )}
